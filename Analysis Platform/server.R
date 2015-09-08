c26eb965ccf2356827d5c734b24e311fb1febb72
skosowsky <sakosows@ncsu.edu>
2015-09-08 09:36:44 -0400

    Updated UI
    1) Analysis platform now supports user input for ticker selection
    2) Output charts now have a slider to control date ranges on the x-axis
    3) There is a new plotly output which is interactive - It shows date and
    price values while hovering over data points and it has click/drag to
    zoom capabilities

--- Analysis Platform/server.R
+++ Analysis Platform/server.R
@@ -2,19 +2,24 @@
 if (!require(quantmod)) {
   stop("This app requires the quantmod package. To install it, run 'install.packages(\"quantmod\")'.\n")
 }
-
-# Download data for a stock if needed, and return the data
-require_symbol <- function(symbol, envir = parent.frame()) {
-  if (is.null(envir[[symbol]])) {
-    envir[[symbol]] <- getSymbols(symbol, auto.assign = FALSE)
-  }
-  
-  envir[[symbol]]
+if (!require(plotly)) {
+  stop("This app requires the plotly package")
 }
+# Download data for a stock if needed, and return the data
+
+
+
 
 
 shinyServer(function(input, output) {
   
+  require_symbol <- function(symbol, envir = parent.frame()) {
+    if (is.null(envir[[symbol]])) {
+      envir[[symbol]] <- getSymbols(symbol,src=input$Data_Source,auto.assign = FALSE,return.class="zoo")
+    }
+    
+    envir[[symbol]]
+  }
   # Create an environment for storing data
   symbol_env <- new.env()
   
@@ -25,14 +30,25 @@ shinyServer(function(input, output) {
     chartSeries(symbol_data,
                 name      = symbol,
                 type      = input$chart_type,
-                subset    = paste(input$daterange, collapse = "::"),
+                subset    = paste(input$Date_Range, collapse = "::"),
                 log.scale = input$log_y,
                 theme     = "white")
   }
+  make_plotly<-function(symbol){
+    symbol_data<-require_symbol(symbol,symbol_env)
+    df<-as.data.frame(symbol_data)
+   p<-plot_ly(y = df[,4], 
+               x = time(symbol_data), 
+               text = paste(time(symbol_data)))
+   layout(p,xaxis=list(title="Date",
+                       range=c(input$Date_Range),type="date",autorange=TRUE)
+          ,yaxis=list(title="Price"))
+  }
+  output$drange<-renderText({as.Date(input$Date_Range)})
   
-  output$plot_aapl <- renderPlot({ make_chart("AAPL") })
-  output$plot_msft <- renderPlot({ make_chart("MSFT") })
-  output$plot_ibm  <- renderPlot({ make_chart("IBM")  })
-  output$plot_goog <- renderPlot({ make_chart("GOOG") })
-  output$plot_yhoo <- renderPlot({ make_chart("YHOO") })
-})
+  output$Ticker_Plot<-renderPlot({make_chart(input$Ticker)})
+
+    output$PLOTLY <- renderPlotly({ make_plotly(input$Ticker)
+   
+  })
+ })
--- Analysis Platform/ui.R
+++ Analysis Platform/ui.R
@@ -3,12 +3,10 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(
     wellPanel(
-      p(strong("Stocks")),
-      checkboxInput(inputId = "stock_aapl", label = "Apple (AAPL)",     value = TRUE),
-      checkboxInput(inputId = "stock_msft", label = "Microsoft (MSFT)", value = FALSE),
-      checkboxInput(inputId = "stock_ibm",  label = "IBM (IBM)",        value = FALSE),
-      checkboxInput(inputId = "stock_goog", label = "Google (GOOG)",    value = TRUE),
-      checkboxInput(inputId = "stock_yhoo", label = "Yahoo (YHOO)",     value = FALSE)
+      p(strong("Stocks"))
+   #  ,checkboxInput(inputId = c("stock_aapl"), label = c("Apple (AAPL)"),  
+   #                 value = TRUE)
+     
     ),
     
     selectInput(inputId = "chart_type",
@@ -18,32 +16,30 @@ shinyUI(pageWithSidebar(
                             "Bar" = "bars",
                             "Line" = "line")
     ),
+   selectInput(inputId = "Data_Source",
+               label = "Data Source",
+               choices = c("Yahoo" = "yahoo",
+                           "Google" = "google")
+   ),
+    textInput("Ticker","Ticker:","CRM"),
     
-    dateRangeInput(inputId = "daterange", label = "Date range",
-                   start = Sys.Date() - 365, end = Sys.Date()),
+    hr(),
+
+    #dateRangeInput(inputId = "daterange", label = "Date range",
+    #               start = Sys.Date() - 365, end = Sys.Date()),
     
     checkboxInput(inputId = "log_y", label = "log y axis", value = FALSE)
   ),
   
   mainPanel(
-    conditionalPanel(condition = "input.stock_aapl",
-                     br(),
-                     div(plotOutput(outputId = "plot_aapl"))),
-    
-    conditionalPanel(condition = "input.stock_msft",
-                     br(),
-                     div(plotOutput(outputId = "plot_msft"))),
-    
-    conditionalPanel(condition = "input.stock_ibm",
-                     br(),
-                     div(plotOutput(outputId = "plot_ibm"))),
-    
-    conditionalPanel(condition = "input.stock_goog",
-                     br(),
-                     div(plotOutput(outputId = "plot_goog"))),
-    
-    conditionalPanel(condition = "input.stock_yhoo",
-                     br(),
-                     plotOutput(outputId = "plot_yhoo"))
+    plotOutput("Ticker_Plot"),
+    sliderInput("Date_Range",
+                label="Date Range",
+                min=Sys.Date()-365,
+                max=Sys.Date(),
+                value=c(Sys.Date()-365,Sys.Date())),
+    plotlyOutput("PLOTLY"),
+    textOutput("drange")
+   
   )
 ))
